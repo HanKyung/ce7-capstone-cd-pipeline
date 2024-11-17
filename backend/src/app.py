c6a5bd3b36fa0ad28e4ee098ea20ca17bfea89e2
HanKyung <infohk.286@gmail.com>
2024-11-17 15:59:45 +0800

    first commit

---
+++ backend/src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# create db instance
+db = SQLAlchemy(app)
+
+import routes
+
+with app.app_context():
+    db.create_all()
+
+test_response = [{'id': 1, 'name': 'Best Friend'}]
+@app.route("/test")
+def send_test_response():
+    return jsonify(test_response)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
